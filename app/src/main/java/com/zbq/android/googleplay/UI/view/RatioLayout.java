753da23f8a077dae1f4cec87d587ccd5b15eb683
zbq <645286197@qq.com>
2016-10-30 10:14:41 +0800

    自定义控件 根据比例调整控件宽高

--- app/src/main/java/com/zbq/android/googleplay/UI/view/RatioLayout.java
+++ app/src/main/java/com/zbq/android/googleplay/UI/view/RatioLayout.java
@@ -12,7 +12,7 @@ import com.zbq.android.googleplay.R;
  * Created by zhangbingquan on 2016/10/29.
  */
 
-public class RatioLayout extends FrameLayout  {
+public class RatioLayout extends FrameLayout {
 
     private float mRatio;
 
@@ -27,13 +27,33 @@ public class RatioLayout extends FrameLayout  {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.RatioLayout);
         mRatio = typedArray.getFloat(R.styleable.RatioLayout_ratio, -1);
         typedArray.recycle();
-        Log.d("typedArray", "RatioLayout: "+mRatio);
+        Log.d("typedArray", "RatioLayout: " + mRatio);
     }
 
     public RatioLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //获取宽度
+        //根据比例计算控件高度
+        //重新测量控件
 
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height;
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        if (widthMode == MeasureSpec.EXACTLY && heightMode != MeasureSpec.EXACTLY && mRatio > 0) {
 
+            int imageWidth = width - getPaddingLeft() - getPaddingRight();
+
+            int imageHeight = (int) (imageWidth / mRatio + 0.5f);
+
+            height = imageHeight + getPaddingTop() + getPaddingBottom();
+
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
 }
